962461a2d9317f6b9a353889aed23b3576fd60a0
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-23 14:55:06 +0200

    Phrase

--- php-badwords.php
+++ php-badwords.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+$var = 'Php is the best'
+?>
+
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP / Badwords</title>
 </head>
 <body>
+
+<h1> Best Code ? : <?php echo $var ?> </h1>
     
 </body>
 </html>
